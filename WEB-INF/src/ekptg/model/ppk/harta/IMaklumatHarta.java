4d920f94371c6b1510e8b55fb5dca20f47ad3d38
Mohamad Rosli <roslizakaria@gmail.com>
2020-03-01 23:04:07 +0800

    tambah interface harta

--- WEB-INF/src/ekptg/model/ppk/harta/IMaklumatHarta.java
+++ WEB-INF/src/ekptg/model/ppk/harta/IMaklumatHarta.java
@@ -2,20 +2,33 @@ package ekptg.model.ppk.harta;
 
 import java.util.Hashtable;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import ekptg.model.ppk.FrmPrmhnnSek8InternalData;
 
 public interface IMaklumatHarta{	
 	
-	public void getMaklumtHarta(org.apache.velocity.VelocityContext context
-		,HttpSession session
-		,Hashtable hParam
-		,String mode
-		,String bolehsimpan
-		,int idNegeri
+	public Hashtable<String,String> setSocParamValues(String negeri
+		,String daerah
+		,String mukim
+		,String bandar
+		,String jenisHakmilik
+		,String kat
+		,String luas
+		,String pemilikan) throws Exception ;
+	public void getHarta(String mode
+		,Hashtable<String,String> hParam
 		,FrmPrmhnnSek8InternalData logic_internal
-			) throws Exception ;
+		,HttpServletRequest request
+		,HttpSession session
+		,org.apache.velocity.VelocityContext context) throws Exception;	
+	public void getHTA(String mode
+		,Hashtable hParam
+		,FrmPrmhnnSek8InternalData logic_internal
+		,HttpServletRequest request
+		,HttpSession session
+		,org.apache.velocity.VelocityContext context) throws Exception;
 	
 	
 }
