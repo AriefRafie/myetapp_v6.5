62580c4fd3f7d55c60e681ed5cf00a9b63155bf8
Mohamad Rosli <roslizakaria@gmail.com>
2020-04-14 22:20:38 +0800

    mt register

---
+++ WEB-INF/src_integrasi/integrasi/ws/mt/reg/Civilregistercaseresponse.java
@@ -0,0 +1,229 @@
+/**
+ * Civilregistercaseresponse.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis 1.4 Apr 22, 2006 (06:55:48 PDT) WSDL2Java emitter.
+ */
+
+package integrasi.ws.mt.reg;
+
+public class Civilregistercaseresponse  implements java.io.Serializable {
+    private java.lang.String code;
+
+    private java.lang.String description;
+
+    private java.lang.String detail;
+
+    private DataCreateRepType data;
+
+    public Civilregistercaseresponse() {
+    }
+
+    public Civilregistercaseresponse(
+           java.lang.String code,
+           java.lang.String description,
+           java.lang.String detail,
+           integrasi.ws.mt.reg.DataCreateRepType data) {
+           this.code = code;
+           this.description = description;
+           this.detail = detail;
+           this.data = data;
+    }
+
+
+    /**
+     * Gets the code value for this Civilregistercaseresponse.
+     * 
+     * @return code
+     */
+    public java.lang.String getCode() {
+        return code;
+    }
+
+
+    /**
+     * Sets the code value for this Civilregistercaseresponse.
+     * 
+     * @param code
+     */
+    public void setCode(java.lang.String code) {
+        this.code = code;
+    }
+
+
+    /**
+     * Gets the description value for this Civilregistercaseresponse.
+     * 
+     * @return description
+     */
+    public java.lang.String getDescription() {
+        return description;
+    }
+
+
+    /**
+     * Sets the description value for this Civilregistercaseresponse.
+     * 
+     * @param description
+     */
+    public void setDescription(java.lang.String description) {
+        this.description = description;
+    }
+
+
+    /**
+     * Gets the detail value for this Civilregistercaseresponse.
+     * 
+     * @return detail
+     */
+    public java.lang.String getDetail() {
+        return detail;
+    }
+
+
+    /**
+     * Sets the detail value for this Civilregistercaseresponse.
+     * 
+     * @param detail
+     */
+    public void setDetail(java.lang.String detail) {
+        this.detail = detail;
+    }
+
+
+    /**
+     * Gets the data value for this Civilregistercaseresponse.
+     * 
+     * @return data
+     */
+    public DataCreateRepType getData() {
+        return data;
+    }
+
+
+    /**
+     * Sets the data value for this Civilregistercaseresponse.
+     * 
+     * @param data
+     */
+    public void setData(DataCreateRepType data) {
+        this.data = data;
+    }
+
+    private java.lang.Object __equalsCalc = null;
+    public synchronized boolean equals(java.lang.Object obj) {
+        if (!(obj instanceof Civilregistercaseresponse)) return false;
+        Civilregistercaseresponse other = (Civilregistercaseresponse) obj;
+        if (obj == null) return false;
+        if (this == obj) return true;
+        if (__equalsCalc != null) {
+            return (__equalsCalc == obj);
+        }
+        __equalsCalc = obj;
+        boolean _equals;
+        _equals = true && 
+            ((this.code==null && other.getCode()==null) || 
+             (this.code!=null &&
+              this.code.equals(other.getCode()))) &&
+            ((this.description==null && other.getDescription()==null) || 
+             (this.description!=null &&
+              this.description.equals(other.getDescription()))) &&
+            ((this.detail==null && other.getDetail()==null) || 
+             (this.detail!=null &&
+              this.detail.equals(other.getDetail()))) &&
+            ((this.data==null && other.getData()==null) || 
+             (this.data!=null &&
+              this.data.equals(other.getData())));
+        __equalsCalc = null;
+        return _equals;
+    }
+
+    private boolean __hashCodeCalc = false;
+    public synchronized int hashCode() {
+        if (__hashCodeCalc) {
+            return 0;
+        }
+        __hashCodeCalc = true;
+        int _hashCode = 1;
+        if (getCode() != null) {
+            _hashCode += getCode().hashCode();
+        }
+        if (getDescription() != null) {
+            _hashCode += getDescription().hashCode();
+        }
+        if (getDetail() != null) {
+            _hashCode += getDetail().hashCode();
+        }
+        if (getData() != null) {
+            _hashCode += getData().hashCode();
+        }
+        __hashCodeCalc = false;
+        return _hashCode;
+    }
+
+    // Type metadata
+    private static org.apache.axis.description.TypeDesc typeDesc =
+        new org.apache.axis.description.TypeDesc(Civilregistercaseresponse.class, true);
+
+    static {
+        typeDesc.setXmlType(new javax.xml.namespace.QName("https://eip.kehakiman.gov.my/services/", ">civilregistercaseresponse"));
+        org.apache.axis.description.ElementDesc elemField = new org.apache.axis.description.ElementDesc();
+        elemField.setFieldName("code");
+        elemField.setXmlName(new javax.xml.namespace.QName("", "code"));
+        elemField.setXmlType(new javax.xml.namespace.QName("http://www.w3.org/2001/XMLSchema", "string"));
+        elemField.setNillable(false);
+        typeDesc.addFieldDesc(elemField);
+        elemField = new org.apache.axis.description.ElementDesc();
+        elemField.setFieldName("description");
+        elemField.setXmlName(new javax.xml.namespace.QName("", "description"));
+        elemField.setXmlType(new javax.xml.namespace.QName("http://www.w3.org/2001/XMLSchema", "string"));
+        elemField.setNillable(false);
+        typeDesc.addFieldDesc(elemField);
+        elemField = new org.apache.axis.description.ElementDesc();
+        elemField.setFieldName("detail");
+        elemField.setXmlName(new javax.xml.namespace.QName("", "Detail"));
+        elemField.setXmlType(new javax.xml.namespace.QName("http://www.w3.org/2001/XMLSchema", "string"));
+        elemField.setMinOccurs(0);
+        elemField.setNillable(false);
+        typeDesc.addFieldDesc(elemField);
+        elemField = new org.apache.axis.description.ElementDesc();
+        elemField.setFieldName("data");
+        elemField.setXmlName(new javax.xml.namespace.QName("", "data"));
+        elemField.setXmlType(new javax.xml.namespace.QName("https://eip.kehakiman.gov.my/services/", "dataCreateRepType"));
+        elemField.setMinOccurs(0);
+        elemField.setNillable(false);
+        typeDesc.addFieldDesc(elemField);
+    }
+
+    /**
+     * Return type metadata object
+     */
+    public static org.apache.axis.description.TypeDesc getTypeDesc() {
+        return typeDesc;
+    }
+
+    /**
+     * Get Custom Serializer
+     */
+    public static org.apache.axis.encoding.Serializer getSerializer(
+           java.lang.String mechType, 
+           java.lang.Class _javaType,  
+           javax.xml.namespace.QName _xmlType) {
+        return 
+          new  org.apache.axis.encoding.ser.BeanSerializer(
+            _javaType, _xmlType, typeDesc);
+    }
+
+    /**
+     * Get Custom Deserializer
+     */
+    public static org.apache.axis.encoding.Deserializer getDeserializer(
+           java.lang.String mechType, 
+           java.lang.Class _javaType,  
+           javax.xml.namespace.QName _xmlType) {
+        return 
+          new  org.apache.axis.encoding.ser.BeanDeserializer(
+            _javaType, _xmlType, typeDesc);
+    }
+
+}
