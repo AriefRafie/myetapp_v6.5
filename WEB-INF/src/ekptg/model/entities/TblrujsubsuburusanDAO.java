72a0aa04069016e3d9e20a04ea31604b700a74a2
Nurul Ain <nurulain.siprotech@gmail.com>
2020-04-17 15:10:01 +0800

    Add new entities for TblrujSubsuburusan

---
+++ WEB-INF/src/ekptg/model/entities/TblrujsubsuburusanDAO.java
@@ -0,0 +1,162 @@
+package ekptg.model.entities;
+
+import java.util.List;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.hibernate.LockMode;
+import org.hibernate.Query;
+import org.hibernate.criterion.Example;
+
+/**
+ * A data access object (DAO) providing persistence and search support for
+ * Tblrujsubsuburusan entities. Transaction control of the save(), update() and
+ * delete() operations can directly support Spring container-managed
+ * transactions or they can be augmented to handle user-managed Spring
+ * transactions. Each of these methods provides additional information for how
+ * to configure it for the desired type of transaction control.
+ * 
+ * @see ekptg.model.entities.Tblrujsubsuburusan
+ * @author MyEclipse Persistence Tools
+ */
+
+public class TblrujsubsuburusanDAO extends BaseHibernateDAO {
+	private static final Log log = LogFactory.getLog(TblrujsubsuburusanDAO.class);
+	// property constants
+	public static final String ID_SUBURUSAN = "idSuburusan";
+	public static final String KOD_SUBSUBURUSAN = "kodSubsuburusan";
+	public static final String NAMA_SUBSUBURUSAN = "namaSubsuburusan";
+	public static final String ID_MASUK = "idMasuk";
+	public static final String ID_KEMASKINI = "idKemaskini";
+
+	public void save(Tblrujsubsuburusan transientInstance) {
+		log.debug("saving Tblrujsubsuburusan instance");
+		try {
+			getSession().save(transientInstance);
+			log.debug("save successful");
+		} catch (RuntimeException re) {
+			log.error("save failed", re);
+			throw re;
+		}
+	}
+
+	public void delete(Tblrujsubsuburusan persistentInstance) {
+		log.debug("deleting Tblrujsubsuburusan instance");
+		try {
+			getSession().delete(persistentInstance);
+			log.debug("delete successful");
+		} catch (RuntimeException re) {
+			log.error("delete failed", re);
+			throw re;
+		}
+	}
+
+	public Tblrujsubsuburusan findById(java.lang.Long id) {
+		log.debug("getting Tblrujsubsuburusan instance with id: " + id);
+		try {
+			Tblrujsubsuburusan instance = (Tblrujsubsuburusan) getSession().get(
+					"ekptg.model.entities.Tblrujsubsuburusan", id);
+			return instance;
+		} catch (RuntimeException re) {
+			log.error("get failed", re);
+			throw re;
+		}
+	}
+
+	public List findByExample(Tblrujsubsuburusan instance) {
+		log.debug("finding Tblrujsubsuburusan instance by example");
+		try {
+			List results = getSession().createCriteria(
+					"ekptg.model.entities.Tblrujsubsuburusan").add(
+					Example.create(instance)).list();
+			log.debug("find by example successful, result size: "
+					+ results.size());
+			return results;
+		} catch (RuntimeException re) {
+			log.error("find by example failed", re);
+			throw re;
+		}
+	}
+
+	public List findByProperty(String propertyName, Object value) {
+		log.debug("finding Tblrujsubsuburusan instance with property: "
+				+ propertyName + ", value: " + value);
+		try {
+			String queryString = "from Tblrujsubsuburusan as model where model."
+					+ propertyName + "= ?";
+			Query queryObject = getSession().createQuery(queryString);
+			queryObject.setParameter(0, value);
+			return queryObject.list();
+		} catch (RuntimeException re) {
+			log.error("find by property name failed", re);
+			throw re;
+		}
+	}
+
+	public List findByIdUrusan(Object idSubrusan) {
+		return findByProperty(ID_SUBURUSAN, idSubrusan);
+	}
+
+	public List findByKodSuburusan(Object kodSubsuburusan) {
+		return findByProperty(KOD_SUBSUBURUSAN, kodSubsuburusan);
+	}
+
+	public List findByNamaSuburusan(Object namaSubsuburusan) {
+		return findByProperty(NAMA_SUBSUBURUSAN, namaSubsuburusan);
+	}
+
+	public List findByIdMasuk(Object idMasuk) {
+		return findByProperty(ID_MASUK, idMasuk);
+	}
+
+	public List findByIdKemaskini(Object idKemaskini) {
+		return findByProperty(ID_KEMASKINI, idKemaskini);
+	}
+
+	public List findAll() {
+		log.debug("finding all Tblrujsubsuburusan instances");
+		try {
+			String queryString = "from Tblrujsubsuburusan";
+			Query queryObject = getSession().createQuery(queryString);
+			return queryObject.list();
+		} catch (RuntimeException re) {
+			log.error("find all failed", re);
+			throw re;
+		}
+	}
+
+	public Tblrujsubsuburusan merge(Tblrujsubsuburusan detachedInstance) {
+		log.debug("merging Tblrujsubsuburusan instance");
+		try {
+			Tblrujsubsuburusan result = (Tblrujsubsuburusan) getSession().merge(
+					detachedInstance);
+			log.debug("merge successful");
+			return result;
+		} catch (RuntimeException re) {
+			log.error("merge failed", re);
+			throw re;
+		}
+	}
+
+	public void attachDirty(Tblrujsubsuburusan instance) {
+		log.debug("attaching dirty Tblrujsubsuburusan instance");
+		try {
+			getSession().saveOrUpdate(instance);
+			log.debug("attach successful");
+		} catch (RuntimeException re) {
+			log.error("attach failed", re);
+			throw re;
+		}
+	}
+
+	public void attachClean(Tblrujsubsuburusan instance) {
+		log.debug("attaching clean Tblrujsubsuburusan instance");
+		try {
+			getSession().lock(instance, LockMode.NONE);
+			log.debug("attach successful");
+		} catch (RuntimeException re) {
+			log.error("attach failed", re);
+			throw re;
+		}
+	}
+}
