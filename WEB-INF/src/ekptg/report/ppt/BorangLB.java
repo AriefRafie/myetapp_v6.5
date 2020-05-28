727ca5b07c28168e4875030944611d609af2addd
Alin <syazlin@si-protech.com.my>
2020-05-28 13:58:40 +0800

    BorangLA, BorangLB, BorangLC, BuktiPenyampaianL, SuratIringanAfidavit, senaraiKehadiranKosong

---
+++ WEB-INF/src/ekptg/report/ppt/BorangLB.java
@@ -0,0 +1,47 @@
+package ekptg.report.ppt;
+
+
+import java.util.Map;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
+
+import ekptg.report.EkptgReportServlet;
+
+public class BorangLB extends EkptgReportServlet{
+	static Logger myLogger = Logger.getLogger(BorangLB.class);
+	
+	
+	public BorangLB() {
+		
+		super.setReportName("BorangLB");
+		super.setFolderName("ppt");
+	}
+
+	public void doProcessing(HttpServletRequest request,
+			HttpServletResponse response, ServletContext context, Map parameters)
+			throws Exception {
+		/*
+		String idfail = "";
+		if (parameters.get("id_fail") != null){
+			idfail = (String)parameters.get("id_fail");
+		}
+		
+		String nofail = "";
+		if (parameters.get("no_fail") != null){
+			nofail = (String)parameters.get("no_fail");
+		}
+		
+		String flagVersion = (String)parameters.get("flagVersion");
+		doVersioning("Borang_LA",idfail,nofail,flagVersion);
+		*/
+	}
+	
+	
+
+	
+	
+}
