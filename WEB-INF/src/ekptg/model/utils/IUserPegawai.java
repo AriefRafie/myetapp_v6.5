28a28d9046b3f80838170614013251b343e03c02
ros <ros@hla-group.com>
2020-08-13 12:41:14 +0800

    tukar guna blank

--- WEB-INF/src/ekptg/model/utils/IUserPegawai.java
+++ WEB-INF/src/ekptg/model/utils/IUserPegawai.java
@@ -12,19 +12,18 @@ public interface IUserPegawai {
 
 	public Hashtable<String, String> getSenaraiPegawai(String idPegawai) throws Exception;
 	public Hashtable<String, String> getPengguna(String idPengguna) throws Exception;
-	
+
 	public List<Map<String,String>> getPenggunaMengikutRole(String role,String idRujukan) throws Exception;
 
-	public Vector<Hashtable<String, String>> getSenaraiPegawai(String idSeksyen,String idSuburusan,String idNegeri) 
-		throws Exception ;	
+	public Vector<Hashtable<String, String>> getSenaraiPegawai(String idSeksyen,String idSuburusan,String idNegeri)
+		throws Exception ;
 	public Vector<Hashtable<String, String>> getSenaraiPegawai(int idNegeri
 		,String idUnit,String tahun)	throws Exception;
 	public Vector<Hashtable<String, String>> getSenaraiPegawaiMapping(String idNegeri) throws Exception;
 	public Vector<Hashtable<String, String>> getSenaraiUsersByNegeri(String idNegeri) throws Exception;
-	
+
 	//public HakmilikUrusan simpanHakmilik(HakmilikUrusan urusan);
 	//public HakmilikUrusan updateHakmilik(HakmilikUrusan urusan);
 	//public void deleteHakmilik(String idHakmilikUrusan) throws Exception;
-
 }
 
