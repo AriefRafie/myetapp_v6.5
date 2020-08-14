2cd7a797dea1b74a4a1e07745d3936d67304672e
cikyatiey <cikyatiey@gmail.com>
2020-08-14 10:27:21 +0800

    php : emel

--- WEB-INF/src/ekptg/model/utils/emel/IEmel.java
+++ WEB-INF/src/ekptg/model/utils/emel/IEmel.java
@@ -1,13 +1,5 @@
 package ekptg.model.utils.emel;
 
-<<<<<<< HEAD
-//yati 13/8/2020
-public interface IEmel {	
-	
-	public String setKandungan(String tajuk,String daripada); 
-	public String setKandungan(String tajuk,String daripada,String noRujukan); 
-	public String seTajuk(String namaSubmodul); 
-=======
 
 public interface IEmel {
 
@@ -16,7 +8,6 @@ public interface IEmel {
 	public String setKandungan(String tajuk,String daripada);
 	public String setKandungan(String tajuk,String daripada,String noRujukan);
 	public String seTajuk(String namaSubmodul);
->>>>>>> a047f985e3b41aa215eb09e873b864fdede17840
 	public String checkEmail(String userId) throws Exception;
 	public String setEmailSign(String noFail);
 	public String setEmailSign(String noRujukan,String tajuk, String dari) ;
