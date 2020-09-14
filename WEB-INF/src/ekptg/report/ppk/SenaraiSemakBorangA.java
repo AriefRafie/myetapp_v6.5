4e7576736a564d31fd905101304464f38a634fa3
nursyafiqahdahari <nursyafiqahdahari@gmail.com>
2020-09-14 10:27:44 +0800

    jkptg online senarai semak borang a

---
+++ WEB-INF/src/ekptg/report/ppk/SenaraiSemakBorangA.java
@@ -0,0 +1,22 @@
+package ekptg.report.ppk;
+
+import java.util.Map;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import ekptg.report.EkptgReportServlet;
+
+public class SenaraiSemakBorangA extends EkptgReportServlet {
+	
+	public SenaraiSemakBorangA() {
+		super.setReportName("SenaraiSemakSek8");
+		super.setFolderName("ppk");
+	}
+
+	public void doProcessing(HttpServletRequest request,
+			HttpServletResponse response, ServletContext context, Map parameters)
+			throws Exception {
+	}
+}
