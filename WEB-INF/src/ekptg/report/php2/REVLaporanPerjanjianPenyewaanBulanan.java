5392637d28a88231aa78560c653694f7b3d0f566
ros <ros@hla-group.com>
2020-11-05 10:10:31 +0800

    php

---
+++ WEB-INF/src/ekptg/report/php2/REVLaporanPerjanjianPenyewaanBulanan.java
@@ -0,0 +1,23 @@
+package ekptg.report.php2;
+
+import java.util.Map;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import ekptg.report.EkptgReportServlet;
+
+public class REVLaporanPerjanjianPenyewaanBulanan extends EkptgReportServlet {
+
+	public REVLaporanPerjanjianPenyewaanBulanan () {
+		super.setReportName("REVLaporanPerjanjianPenyewaanBulan");
+		super.setFolderName("php2\\REV\\LAPORAN");
+	}
+
+	@Override
+	public void doProcessing(HttpServletRequest request,
+			HttpServletResponse response, ServletContext context, Map parameters)
+			throws Exception {
+	}
+}
