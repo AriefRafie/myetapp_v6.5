036e7bc6d06dac2e17c617b2266894c35f48cc42
norshafiqa <norshafiqa@si-protect.com.my>
2020-11-26 04:40:40 +0800

    KJP-Pembelian

--- WEB-INF/src/ekptg/model/htp/pembelian/IPembelianOnline.java
+++ WEB-INF/src/ekptg/model/htp/pembelian/IPembelianOnline.java
@@ -5,7 +5,7 @@ import java.util.Vector;
 import ekptg.model.htp.entity.HtpPermohonan;
 
 public interface IPembelianOnline {
-	public Vector findFailKJP(String carian, String noFail, String idNegeri
+	public Vector findFailKJP(String carian, String noFail, String noFailOnline, String idNegeri
 			,String idKementerian,String idAgensi,String idSubUrusan,String idStatus);
 	
 	public HtpPermohonan kemaskiniPermohonan(HtpPermohonan htpPermohonan,String ipPermohonan,String idHtpPermohonan)throws Exception;
