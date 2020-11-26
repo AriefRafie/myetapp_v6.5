78339175a87539f02b39f6889ccf19d0c6db3a4d
muhammad.arief <muhammad.arief@si-protech.com.my>
2020-11-26 22:31:10 +0800

    update tukar pegawai 2

--- WEB-INF/src/ekptg/model/ppk/FrmTukarPegawaiBI2Data.java
+++ WEB-INF/src/ekptg/model/ppk/FrmTukarPegawaiBI2Data.java
@@ -44,9 +44,9 @@ public class FrmTukarPegawaiBI2Data {
 
 /*
  * List fail tukar pegawai 2:
- * 1. ekptg.view.ppk.FrmTukarPegawaiBI2
+ * 1. ekptg.view.ppk.FrmTukarPegawaiBI2.java
  * 2. app/ppk/TukarPegawai2/FrmPrmhnnTukarPegawai2.jsp
  * 3. app/ppk/TukarPegawai2/historyTukarPegawai2.jsp
  * 4. app/ppk/TukarPegawai2/listTukarPegawai2.jsp
- * 5. ekptg.model.ppk.FrmTukarPegawaiBI2Data
+ * 5. ekptg.model.ppk.FrmTukarPegawaiBI2Data.java
  * */
