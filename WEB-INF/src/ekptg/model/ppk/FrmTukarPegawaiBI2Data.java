b5eee321e0dfc85f4e54cb5c1a9db9cf3c2199fe
muhammad.arief <muhammad.arief@si-protech.com.my>
2020-11-27 14:24:55 +0800

    update tukar pegawai 2

--- WEB-INF/src/ekptg/model/ppk/FrmTukarPegawaiBI2Data.java
+++ WEB-INF/src/ekptg/model/ppk/FrmTukarPegawaiBI2Data.java
@@ -39,6 +39,99 @@ import ekptg.report.ppk.FrmPopupPilihPegawaiReportData;
 public class FrmTukarPegawaiBI2Data {
 	static Logger myLogger = Logger.getLogger(ekptg.model.ppk.FrmTukarPegawaiBI2Data.class);
 	
+	public Map getDetailUsers(HttpSession session, String nama, String user_id, String id_negeri, Db db)throws Exception {
+		Db db1 = null;
+		ResultSet rs = null;
+		Statement stmt = null;
+		String sql = "";
+		try {
+			if(db != null)
+			{
+				db1 = db;
+			}
+			else
+			{
+				db1 = new Db();
+			}
+			stmt = db1.getStatement();	
+			sql += " SELECT * FROM USERS U,USERS_INTERNAL UI WHERE U.USER_ID = UI.USER_ID " +
+					//" AND NVL(UI.FLAG_AKTIF,' ') IN (' ','1') " +
+					//" AND UI.ID_SEKSYEN = '2' " +
+					" ";
+			if(!user_id.equals(""))
+			{
+				sql += " AND U.USER_ID = '"+user_id+"' ";
+			}
+			else
+			{
+				if(!nama.equals(""))
+				{
+					sql += " AND UPPER(U.USER_NAME) = '"+nama.toUpperCase()+"' ";
+				}
+			}
+			
+			if(!id_negeri.equals(""))
+			{
+				sql += " AND UI.ID_NEGERI = '"+id_negeri+"' ";
+			}
+			
+			myLogger.info(" Tukar Pegawa Pengendali 2 : SQL getDetailUsers :"+ sql);			
+			rs = stmt.executeQuery(sql);
+			Map h = null;		
+			while (rs.next()) {	
+				h = Collections.synchronizedMap(new HashMap());		
+				h.put("USER_ID",rs == null ? "" :rs.getString("USER_ID") == null ? "" : rs.getString("USER_ID"));
+				h.put("USER_NAME",rs == null ? "" :rs.getString("USER_NAME") == null ? "" : rs.getString("USER_NAME"));
+				h.put("ID_JAWATAN",rs == null ? "" :rs.getString("ID_JAWATAN") == null ? "" : rs.getString("ID_JAWATAN"));
+				h.put("ID_NEGERI",rs == null ? "" :rs.getString("ID_NEGERI") == null ? "" : rs.getString("ID_NEGERI"));		
+				h.put("ID_SEKSYEN",rs == null ? "" :rs.getString("ID_SEKSYEN") == null ? "" : rs.getString("ID_SEKSYEN"));		
+			}
+			return h;
+		} finally {
+			if (db == null)
+			{
+				db1.close();
+			}
+		}
+	}
+	
+	public String getDetailPegawaiList(HttpSession session, String username, Db db)throws Exception {
+		String USER_ID_SYSTEM = (String)session.getAttribute("_ekptg_user_id");	
+		Db db1 = null;
+		ResultSet rs = null;
+		Statement stmt = null;
+		String sql = "";
+		try {
+			if(db != null)
+			{
+				db1 = db;
+			}
+			else
+			{
+				db1 = new Db();
+			}
+			stmt = db1.getStatement();			
+			sql += " SELECT LIST_ID_UNITPSK || (SELECT ',' || (rtrim (xmlagg (xmlelement (e, ID_UNITPSK || ','  )).extract ('//text()'))|| 0) FROM TBLPPKRUJUNIT  WHERE USER_ID = '"+USER_ID_SYSTEM+"'  GROUP BY USER_ID) AS LIST_ID_UNITPSK FROM ( " +
+				   " SELECT (rtrim (xmlagg (xmlelement (e, ID_UNITPSK || ','  )).extract ('//text()')) || 0) " +
+				   " AS LIST_ID_UNITPSK FROM ( "+
+				   " SELECT ID_UNITPSK,NAMA_PEGAWAI, UTL_MATCH.edit_distance(UPPER(NAMA_PEGAWAI),UPPER('"+username+"')) TOT_BEZA "+
+				   " FROM TBLPPKRUJUNIT ) WHERE  TOT_BEZA < 4 ) ";			
+			myLogger.info(" BICARA INTERAKTIF : SQL getDetailPegawaiList :"+ sql);			
+			rs = stmt.executeQuery(sql);
+			
+			String LIST_ID_UNITPSK = "0";
+			while (rs.next()) {	
+				LIST_ID_UNITPSK = rs == null ? "" :rs.getString("LIST_ID_UNITPSK") == null ? "" : rs.getString("LIST_ID_UNITPSK");
+			}
+			return LIST_ID_UNITPSK;
+		} finally {
+			if (db == null)
+			{
+				db1.close();
+			}
+		}
+	}
+	
 }
 
 
